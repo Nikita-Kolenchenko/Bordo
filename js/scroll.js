30fac8c657053b8153f4096368f36ed78852c7a8
Nikita <kolencenkonikita97@gmail.com>
2026-04-11 18:05:46 +0300

    update header

--- js/scroll.js
+++ js/scroll.js
@@ -4,6 +4,34 @@ const ss = document.querySelectorAll("section span");
 const navLinks = document.querySelectorAll(".menu-2 a");
 const subMenus = document.querySelectorAll("#extendedMenu span");
 
+let lastScroll = 0;
+const header = document.querySelector("header"); // или твой класс навигации
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll <= 0) {
+    header.classList.remove("header-hidden");
+    return;
+  }
+
+  if (
+    currentScroll > lastScroll &&
+    !header.classList.contains("header-hidden")
+  ) {
+    // Скроллим вниз — прячем
+    header.classList.add("header-hidden");
+  } else if (
+    currentScroll < lastScroll &&
+    header.classList.contains("header-hidden")
+  ) {
+    // Скроллим вверх — показываем
+    header.classList.remove("header-hidden");
+  }
+
+  lastScroll = currentScroll;
+});
+
 window.addEventListener("scroll", () => {
   let current = "";
 
